978a60252fa3eff8d3e485f828b6cc9d3dc35e01
Blankz <korn_b_za@hotmail.com>
2021-01-30 23:12:15 +0700

    added bonus3

---
+++ Bonus3.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+let firstArray = [17,2,3,5,7,8,11,13,17,19,20]
+let secArray = [19,3,5,7,10,12,15]
+var sum: [Int] = []
+
+for first in firstArray {
+    for sec in secArray {
+        if first == sec {
+            sum.append(first)
+        }
+    }
+}
+sum
