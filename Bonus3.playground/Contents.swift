68aeb43e35b7b720f6812bbbf76a5fb8f424277d
Blankz <korn_b_za@hotmail.com>
2021-01-31 09:36:19 +0700

    updated loop

--- Bonus3.playground/Contents.swift
+++ Bonus3.playground/Contents.swift
@@ -4,11 +4,13 @@ let firstArray = [17,2,3,5,7,8,11,13,17,19,20]
 let secArray = [19,3,5,7,10,12,15]
 var sum: [Int] = []
 
-for first in firstArray {
-    for sec in secArray {
+outer: for first in firstArray {
+    inner: for sec in secArray {
         if first == sec {
             sum.append(first)
+            continue outer
         }
+        print(first)
     }
 }
 sum
